2f80ec5a803bf9dcd2cfe3cd7dfca6806651f28b
Samuel Squire <sam@samsquire.com>
2023-04-24 15:32:28 +0100

    Sam now how do we make it consistent?

--- node.py
+++ node.py
@@ -1,7 +1,9 @@
 import socket
 import select
-from threading import Thread
+from threading import Thread, Lock
 from argparse import ArgumentParser
+import queue
+
 
 from collections import defaultdict
 parser = ArgumentParser()
@@ -9,11 +11,32 @@ parser.add_argument("run", help="One of init, daemon, add-host")
 parser.add_argument("--index", help="index")
 args = parser.parse_args()
 
-class Server(Thread):
-  def __init__(self, port):
-    super(Server, self).__init__()
-    self.port = port
+class Database:
+
+  def __init__(self, server):
     self.database = {}
+    self.server = server
+
+  def persist(self, splitted):
+    if splitted[1] not in self.database:
+      self.database[splitted[1]] = {}
+    saved = self.database[splitted[1]] 
+    # print(self.server, saved)
+    if "value" not in self.database[splitted[1]]:
+      newitem = {}
+      newitem["value"] = [int(splitted[2])]
+      self.database[splitted[1]] = newitem
+    else:
+      self.database[splitted[1]]["value"].append(splitted[2])
+    # print(self.server, self.database[splitted[1]]["value"][-1])
+
+class Server(Thread):
+  def __init__(self, index, port, database, clients):
+    super(Server, self).__init__()
+    self.index = index
+    self.port = port
+    self.database = database
+    self.clients = clients
 
   def run(self):
 
@@ -49,7 +72,14 @@ class Server(Thread):
                       "fd": fd,
                       "pending_send": []
                   }
-                elif event & select.EPOLLIN:
+
+                if fileno != s.fileno() and event & select.EPOLLOUT:
+                    for data in connections[fileno]["pending_send"]:
+                        print("Sending {} to connection ".format(data, fileno))
+                        connections[fileno]["connection"].send(data.encode())
+                    connections[fileno]["pending_send"].clear()
+
+                if fileno != s.fileno() and event & select.EPOLLIN:
                     data = connections[fileno]["connection"].recv(1024)
                     if not data:
                         print("Removing connection")
@@ -61,33 +91,33 @@ class Server(Thread):
                     for data in lines:
                         if not data: continue
                         splitted = data.split(" ")
-                        if splitted[0] == "set" and len(splitted) == 3:
-                          if splitted[1] not in self.database:
-                            self.database[splitted[1]] = {}
-                          saved = self.database[splitted[1]] 
-                          print(self.port, saved)
-                          if "value" not in self.database[splitted[1]]:
-                            newitem = {}
-                            newitem["value"] = [int(splitted[2])]
-                            self.database[splitted[1]] = newitem
-                          else:
-                            self.database[splitted[1]]["value"].append(splitted[2])
+                        print(splitted)
+                        if splitted[0] == "server" and len(splitted) == 2:
+                          print("received server name")
+                          server_index = int(splitted[1])
+                          connections[fileno]["server_index"] = server_index
+                          connections[fileno]["initialized"] = True
+                        if splitted[0] == "sync" and len(splitted) == 3:
+                          self.database.persist(splitted)
+                        elif "initialized" in connections[fileno] and splitted[0] == "set" and len(splitted) == 3:
+                          self.database.persist(splitted)
+                          server_index = connections[fileno]["server_index"]
+                          sender = self.clients[server_index].sender
+                          synced = "sync {} {}\t".format(splitted[1], splitted[2])
+                          sender.queue.put(synced)
 
-                elif fileno != s.fileno() and event & select.EPOLLOUT:
-                    for data in connections[fileno]["pending_send"]:
-                        print("Sending {} to connection ".format(data, fileno))
-                        connections[fileno]["connection"].sendall(data.encode())
-                    connections[fileno]["pending_send"].clear()
+
         e.unregister(args[0])
         e.close()
 
 class Client(Thread):
 
-  def __init__(self, port, index):
+  def __init__(self, port, index, sender):
       super(Client, self).__init__()
       self.HOST = '127.0.0.1'  # The server's hostname or IP address
       self.PORT = port        # The port used by the server
       self.index = index
+      self.sender = sender
 
   def run(self):
       self.running = True
@@ -95,13 +125,13 @@ class Client(Thread):
         try:
           self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           self.s.connect((self.HOST, self.PORT))
-          self.s.send(str(self.index).encode('utf8'))
-          self.running = False
+          success = self.s.send("server {}".format(self.index).encode('utf8'))
+          self.running = not success
         except:
           pass # print("Waiting for server {}".format(self.PORT))
 
 class ClientReceiver(Thread):
-  def __init__(self, client, port):
+  def __init__(self, client, port, database):
      super(ClientReceiver, self).__init__()
      self.client = client
      self.packets = []
@@ -112,58 +142,74 @@ class ClientReceiver(Thread):
       while self.running:
         try:
           data = self.client.s.recv(1024)
+           
           if not data:
               self.running = False
-          datas = data.decode('utf8').split("\n")
-          for line in datas:
-            print("Received " + line) 
+          
+          splitted = data.decode('utf8').split(" ")
+          # print(splitted)
+          database.persist(splitted) 
+
         except:
           pass
 
 
 class ClientSender(Thread):
-  def __init__(self, client, port):
+  def __init__(self, port):
      super(ClientSender, self).__init__()
-     self.client = client
      self.packets = []
      self.running = True
+     self.port = port
+     self.queue = queue.Queue()
+
   def run(self):
+    while self.running:
+      try:
+        item = self.queue.get()
+        # print("Sending {} to port {}".format(item, self.port))
+        error = self.client.s.send(item.encode('utf8'))
+        self.queue.task_done() 
+      except:
+        pass
 
-      while self.running:
-        for item in self.packets:
-          try:
-            # print("Sending packet")
-            error = self.client.s.send(item.encode('utf8'))
-            if not error:
-              self.running = False
-          except:
-            pass
-        self.packets.clear() 
+servers = [
 
-
-server = Server(65432 + int(args.index))
-server.start()
+]
 clients = []
-for i in range(0, 5):
+database = Database(args.index)
+server = Server(args.index, 65432 + int(args.index), database, clients)
+server.start()
+for i in range(0, 6):
   port = 65432 + i
-  if int(args.index) != port:
-    client = Client(port, args.index)
-    clients.append(client) 
-    client.start() 
-    
-    while client.running:
-      pass
-    receiver = ClientReceiver(client, i) 
-    receiver.start()
-    sender = ClientSender(client, i)
-    sender.start()
-    client.receiver = receiver
-    client.sender = sender
+  sender = ClientSender(i)
+  client = Client(port, args.index, sender)
+  sender.client = client
+  clients.append(client) 
+  
+  receiver = ClientReceiver(client, i, database) 
+  client.receiver = receiver
+  client.sender = sender
+  servers.append({
+    "client": client,
+    "receiver": receiver,
+    "sender": sender,
+  })
     # sender.packets.append("set 0 1")
+
+for c_server in servers:
+    c_server["client"].start() 
+    c_server["sender"].start()
+    c_server["receiver"].start()
 
 print("###########################~")
 counter = 0
-while True:
+import time
+last_time = time.time()
+while round(time.time() - last_time) < 2:
   for client in clients:
-    client.sender.packets.append("set item {}\t".format(counter)) 
+
+    client.sender.queue.put("set item {}\t".format(counter)) 
     counter = counter + 1
+
+
+
